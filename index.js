7234b70c057f299a48a940b8c13a24dea41daf97
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-03-06 10:27:07 +0500

    checkpoint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// logic for first half of tweet
+
+// adding class variables
 const enterName = document.querySelector('.enter-name');
-const prevName = document.querySelector('.name');
-
 const enterUserName = document.querySelector('.enter-username');
-const prevUserName = document.querySelector('.username');
-
 const enterTweet = document.querySelector('.enter-tweet');
+
+const prevName = document.querySelector('.name');
+const prevUserName = document.querySelector('.username');
 const prevTweet = document.querySelector('.message');
 
+// functions for event listeners to change preview
 function addName() {
   prevName.innerHTML = enterName.value.trim();
 }
@@ -19,8 +22,57 @@ function addTweet() {
   prevTweet.innerHTML = enterTweet.value.trim(); // TODO: contain this inside div
 }
 
+// event listeners to change prev
 enterName.addEventListener('input', addName);
 enterUserName.addEventListener('input', addUserName);
 enterTweet.addEventListener('input', addTweet);
 
-// TODO: add all other inputs tmmrw
+// second half of tweet
+
+// adding class variables
+const enterTime = document.querySelector('.enter-time');
+const enterDate = document.querySelector('.enter-date'); // TODO: change format to match twitters'  
+const enterUserDevice = document.querySelector('.enter-userdevice');
+const enterRetweets = document.querySelector('.enter-retweets');
+const enterQuotes = document.querySelector('.enter-quotes');
+const enterLikes = document.querySelector('enter-likes');
+
+const prevTime = document.querySelector('.time');
+const prevDate = document.querySelector('.date');
+const prevUserDevice = document.querySelector('.userdevice');
+const prevRetweets = document.querySelector('.retweets');
+const prevQuotes = document.querySelector('.quotes');
+const prevLikes = document.querySelector('.likes');
+
+// functions for event listeners to change preview
+
+function addTime() {
+  prevTime.innerHTML = enterTime.value.trim();
+}
+
+function addDate() {
+  prevDate.innerHTML = enterDate.value.trim();
+}
+
+function addUserDevice() {
+  prevUserDevice.innerHTML = enterUserDevice.value.trim(); // TODO: add options to this
+}
+
+function addRetweets() {
+  prevRetweets.innerHTML = enterRetweets.value.trim();
+}
+
+function addQuotes() {
+  prevQuotes.innerHTML = enterQuotes.value.trim();
+}
+function addLikes() {
+  prevLikes.innerHTML = enterLikes.value.trim(); // TODO: fix this
+}
+
+// event listeners to change prev
+enterDate.addEventListener('input', addDate);
+enterTime.addEventListener('input', addTime);
+enterUserDevice.addEventListener('input', addUserDevice);
+enterRetweets.addEventListener('input', addRetweets);
+enterQuotes.addEventListener('input', addQuotes);
+enterLikes.addEventListener('input', addLikes);
