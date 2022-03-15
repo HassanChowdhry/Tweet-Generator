074dbcd13fef783a0c24cd9caaaee372d1c03256
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2022-03-15 22:23:33 +0500

    final steps (#10)
    * checkpoint
    
    * fixed userdevice options issue
    
    * checkpoint
    
    * fixed date and time issue
    
    * bit more logic
    
    * added most  logic(#12)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// logic for first half of tweet
+//! logic for first half of tweet
 
 // adding class variables
 const enterName = document.querySelector('.enter-name');
@@ -19,11 +19,8 @@ function addUserName() {
 }
 
 function addTweet() {
-  // if (enterTweet.value.length % 65 === 0) {
-  //   enterTweet.innerHTML = ' <br /> ';
-  // }
   if (enterTweet.value.length < 200) {
-    prevTweet.innerHTML = enterTweet.value.trim(); 
+    prevTweet.innerHTML = enterTweet.value.trim();
   }
 }
 
@@ -32,15 +29,21 @@ enterName.addEventListener('input', addName);
 enterUserName.addEventListener('input', addUserName);
 enterTweet.addEventListener('input', addTweet);
 
-// second half of tweet
+//! second half of tweet
 
 // adding variables for entering details part
 const enterTime = document.querySelector('.enter-time');
-const enterDate = document.querySelector('.enter-date'); // TODO: change format to match twitters'  
+const enterDate = document.querySelector('.enter-date');
 const enterUserDevice = document.querySelector('.enter-userdevice');
 const enterRetweets = document.querySelector('.enter-retweets');
 const enterQuotes = document.querySelector('.enter-quotes');
 const enterLikes = document.querySelector('.enter-likes');
+const enterOtherUserDevice = document.querySelector('.enter-other-userdevice');
+
+const otherUserDeviceDiv = document.querySelector('.other-userdevice');
+const retweetsDiv = document.querySelector('.retweet-div');
+const quotesDiv = document.querySelector('.quotes-div');
+const likesDiv = document.querySelector('.likes-div');
 
 // adding variables for preview part
 const prevTime = document.querySelector('.time');
@@ -52,27 +55,138 @@ const prevLikes = document.querySelector('.likes');
 
 // functions for event listeners to change preview
 function addTime() {
-  prevTime.innerHTML = enterTime.value.trim();
+  let timeArray = enterTime.value;
+  timeArray = timeArray.split(':');
+
+  //* changes to 12 hour format
+  let hour = timeArray[0];
+
+  if (hour === '12') {
+    prevTime.innerHTML = `12:${timeArray[1]} pm`;
+
+  } else if (hour === '00') {
+    prevTime.innerHTML = `12:${timeArray[1]} am`;
+
+  } else if (hour > '12') {
+    timeArray[0] = Math.abs(12 - hour);
+    prevTime.innerHTML = `${timeArray[0]}:${timeArray[1]} pm`;
+
+  } else {
+    if (hour.charAt(0) === '0') {
+      timeArray[0] = hour.slice(1, 2);
+    }
+
+    prevTime.innerHTML = `${timeArray[0]}:${timeArray[1]} am`;
+  }
+}
+
+/**
+ * ? could use switch case
+ * @param {Any[]} dateArray
+ * @returns {Any[]}
+ */
+function monthNumberToWord(dateArray) {
+  let dateMonth = dateArray[1];
+
+  if (dateMonth === '01') {
+    dateArray[1] = 'Jan';
+
+  } else if (dateMonth === '02') {
+    dateArray[1] = 'Feb';
+
+  } else if (dateMonth === '03') {
+    dateArray[1] = 'Mar';
+
+  } else if (dateMonth === '04') {
+    dateArray[1] = 'Apr';
+
+  } else if (dateMonth === '05') {
+    dateArray[1] = 'May';
+
+  } else if (dateMonth === '06') {
+    dateArray[1] = 'Jun';
+
+  } else if (dateMonth === '07') {
+    dateArray[1] = 'Jul';
+
+  } else if (dateMonth === '08') {
+    dateArray[1] = 'Aug';
+
+  } else if (dateMonth === '09') {
+    dateArray[1] = 'Sep';
+
+  } else if (dateMonth === '10') {
+    dateArray[1] = 'Oct';
+
+  } else if (dateMonth === '11') {
+    dateArray[1] = 'Nov';
+
+  } else if (dateMonth === '12') {
+    dateArray[1] = 'Dec';
+  }
+  return dateArray;
 }
 
 function addDate() {
-  prevDate.innerHTML = enterDate.value.trim();
+  let dateArray = enterDate.value;
+  dateArray = dateArray.split('-');
+  dateArray = monthNumberToWord(dateArray);
+  dateArray[0] = dateArray[0].slice(2, 4); //* removes first two numbers from years
+
+  prevDate.innerHTML = `${dateArray[2]} ${dateArray[1]} ${dateArray[0]}`;
 }
 
 function addUserDevice() {
-  prevUserDevice.innerHTML = enterUserDevice.value.trim(); // TODO: add options to this
+  if (enterUserDevice.value !== 'Other') {
+    otherUserDeviceDiv.style.visibility = 'hidden';
+    otherUserDeviceDiv.style.position = 'absolute';
+    prevUserDevice.innerHTML = enterUserDevice.value.trim();
+
+  } else {
+    otherUserDeviceDiv.style.visibility = 'visible';
+    otherUserDeviceDiv.style.position = 'relative';
+
+    enterOtherUserDevice.addEventListener('input', () => {
+      prevUserDevice.innerHTML = enterOtherUserDevice.value.trim();
+    });
+
+  }
 }
 
 function addRetweets() {
-  prevRetweets.innerHTML = enterRetweets.value.trim();
+  if (enterRetweets.value.trim() === '0') {
+    retweetsDiv.style.visibility = 'hidden';
+    retweetsDiv.style.position = 'absolute';
+
+  } else {
+    retweetsDiv.style.visibility = 'visible';
+    retweetsDiv.style.position = 'relative';
+    prevRetweets.innerHTML = enterRetweets.value.trim();
+  }
 }
 
 function addQuotes() {
-  prevQuotes.innerHTML = enterQuotes.value.trim();
+  if (enterQuotes.value.trim() === '0') {
+    quotesDiv.style.visibility = 'hidden';
+    quotesDiv.style.position = 'absolute';
+
+  } else {
+    quotesDiv.style.visibility = 'visible';
+    quotesDiv.style.position = 'relative';
+    prevQuotes.innerHTML = enterQuotes.value.trim();
+  }
 }
 
 function addLikes() {
-  prevLikes.innerHTML = enterLikes.value.trim();
+  if (enterLikes.value.trim() === '0') {
+    likesDiv.style.visibility = 'hidden';
+    likesDiv.style.position = 'absolute';
+
+  } else {
+    likesDiv.style.visibility = 'visible';
+    likesDiv.style.position = 'relative';
+    prevLikes.innerHTML = enterLikes.value.trim();
+  }
 
 }
 
@@ -84,7 +198,7 @@ enterRetweets.addEventListener('input', addRetweets);
 enterQuotes.addEventListener('input', addQuotes);
 enterLikes.addEventListener('input', addLikes);
 
-// Button logic 
+//! Button logic 
 
 // calling them
 const verifiedButton = document.querySelector('.verified');
@@ -96,18 +210,25 @@ const uploadPfpInput = document.querySelector('#pfp-id');
 const prevPfp = document.querySelector('.pfp-image');
 
 function addVerifiedLogo() {
-// could use ternary?? This makes it more simple imo
+
   if (verifiedBadge.style.visibility === 'visible') {
+    verifiedButton.style.background = '#fff';
+    verifiedButton.style.color = '#000';
+
     verifiedBadge.style.visibility = 'hidden';
 
   } else {
+    verifiedButton.style.background = '#000';
+    verifiedButton.style.color = '#fff';
+
     verifiedBadge.style.visibility = 'visible';
+
   }
 }
 
 function uploadPfp() {
   uploadPfpInput.click();
-  
+
   uploadPfpInput.onchange = () => {
     const [file] = uploadPfpInput.files;
     if (file) {
